de827c1252059f8c7a614a5216ed993f46c53b7d
Sirius <igor@MacBook-Pro.local>
2018-10-01 21:17:24 +0300

    Add comment

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ char** remove_spaces(char * const *const mas, const unsigned int size) {
     posix_memalign((void*)array, 8, size * sizeof(char*));
     register unsigned int top;
     for (int i = 0; i < size; ++i) {
-        register bool space = true;
+        register bool space = true; //com
         array[i] = malloc(30);
         top = 0;
         for (int j = 0; j < strlen(mas[i]); ++j) {
