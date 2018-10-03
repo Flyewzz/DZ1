f41caad5cd1cd2cc711705bcf4ccfedc3e8af600
Sirius <igor@MacBook-Pro-3.local>
2018-10-03 11:10:23 +0300

    New version

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Sirius. All rights reserved.
 //
 
+#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,60 +15,57 @@
 
 typedef enum {false, true} bool;
 char** remove_spaces(char * const *const mas, const unsigned int size) {
+    if (size == 0) return NULL;
     char **array = malloc(size * sizeof(char*));
-    posix_memalign((void*)array, 8, size * sizeof(char*));
+  //  posix_memalign((void*)array, 64, size * sizeof(char*));
     register unsigned int top;
     for (int i = 0; i < size; ++i) {
         register bool space = true;
         array[i] = malloc(30);
         top = 0;
-        // Работает!
         for (int j = 0; j < strlen(mas[i]); ++j) {
-            if (mas[i][j] != ' ') {
-                space = false;
-                array[i][top++] = mas[i][j];
+            if (mas[i][j] == ' ') {
+                if (space == true) continue;
+                space = true;
             }
-            else {
-                if (!space) {
-                    array[i][top++] = mas[i][j];
-                    space = true;
-                }
-                else continue;
-            }
+            else space = false;
+            array[i][top++] = mas[i][j];
         }
     }
     return array;
 }
 
-unsigned int MASSIZE = 40;
-unsigned int BUFSIZE = 30;
+size_t MASSIZE = 1;
+size_t BUFSIZE = 2;
 
 int main(int argc, const char * argv[]) {
     
     register unsigned int top = 0;
     register unsigned int cnt = 0;
-    errno = 0;
     char ** mas = malloc(MASSIZE * sizeof(char*));
-    posix_memalign((void*)mas, 8, MASSIZE * sizeof(char*));
-    char *buffer = malloc(BUFSIZE);
-    
-    fgets(buffer, BUFSIZE, stdin);
-    while (strncmp(buffer, "\n", BUFSIZE)) {
+  //  posix_memalign((void*)mas, 64, MASSIZE * sizeof(char*));
+    char *buffer;
+ //   while (strncmp(buffer, "\n", strlen(buffer))) {
+    while (1) {
+        buffer = malloc(BUFSIZE * sizeof(char));
+        getline(&buffer, &BUFSIZE, stdin);
+        buffer = (char*)realloc(buffer, BUFSIZE * sizeof(char));
+        if (!strcmp(buffer, "\n")) { free(buffer); break;}
+        if (top >= MASSIZE)
+            mas = (char**)realloc(mas, ++MASSIZE);
         mas[top++] = buffer;
-        if (errno == ENOMEM) {
-            printf("[error]");
-            return 0;
-        }
         ++cnt;
-        buffer = malloc(BUFSIZE);
-        fgets(buffer, BUFSIZE, stdin);
     }
+
 char **result = remove_spaces(mas, cnt);
-    for (int i = 0; i < cnt; ++i)
-        printf("%s\n", result[i]);
-    free(buffer);
+    for (int i = 0; i < cnt; ++i) {
+     printf("%s\n", result[i]);
+        free(result[i]);
+    }
     for (int i = 0; i < cnt; ++i) free(mas[i]);
     free(mas);
-    free(result);
+    if (result == NULL) printf("[error]\n");
+    else
+        free(result);
     return 0;
 }
