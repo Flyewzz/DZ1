614f818c64176c861645091064181b1e234b851e
Sirius <igor@MacBook-Pro-3.local>
2018-10-03 14:07:54 +0300

    Correct!

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ typedef enum {false, true} bool;
 char** remove_spaces(char * const *const mas, const unsigned int size) {
     if (size == 0) return NULL;
     char **array = malloc(size * sizeof(char*));
-  //  posix_memalign((void*)array, 64, size * sizeof(char*));
+   // posix_memalign((void*)array, 64, size * sizeof(char*));
     register unsigned int top;
     for (int i = 0; i < size; ++i) {
         register bool space = true;
@@ -43,16 +43,15 @@ int main(int argc, const char * argv[]) {
     register unsigned int top = 0;
     register unsigned int cnt = 0;
     char ** mas = malloc(MASSIZE * sizeof(char*));
-  //  posix_memalign((void*)mas, 64, MASSIZE * sizeof(char*));
+   // posix_memalign((void*)mas, 64, MASSIZE * sizeof(char*));
     char *buffer;
  //   while (strncmp(buffer, "\n", strlen(buffer))) {
     while (1) {
         buffer = malloc(BUFSIZE * sizeof(char));
         getline(&buffer, &BUFSIZE, stdin);
-        buffer = (char*)realloc(buffer, BUFSIZE * sizeof(char));
         if (!strcmp(buffer, "\n")) { free(buffer); break;}
         if (top >= MASSIZE)
-            mas = (char**)realloc(mas, ++MASSIZE);
+            mas = (char**)realloc(mas, ++MASSIZE*sizeof(char*));
         mas[top++] = buffer;
         ++cnt;
     }
